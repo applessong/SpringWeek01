f49d8930711054040dc3b4e182decf2fd49e5f99
applessong <ok3565z@naver.com>
2022-07-01 19:41:46 +0900

    Initial commit

---
+++ src/main/java/shop/applesong/springweek01/SpringWeek01Application.java
@@ -0,0 +1,13 @@
+package shop.applesong.springweek01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWeek01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWeek01Application.class, args);
+    }
+
+}
---
+++ src/main/java/shop/applesong/springweek01/prac/first.java
@@ -0,0 +1,10 @@
+package shop.applesong.springweek01.prac;
+
+public class first {
+    public static void main(String[] args) {
+        System.out.println("안녕, 스파르타!");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
